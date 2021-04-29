3cbaad0c9a16903d17eef5407cca3dcc3269cd82
aelseewy <74848911+aelseewy@users.noreply.github.com>
2021-04-29 03:10:45 +0200

    chaned set

--- hanaablog/settings.py
+++ hanaablog/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hanaablog_data',
-        'USER': 'postgress',
+        'USER': 'hanaablog',
         'PASSWORD': 'Salamat',
         'HOST': 'localhost',
         'PORT': '',
